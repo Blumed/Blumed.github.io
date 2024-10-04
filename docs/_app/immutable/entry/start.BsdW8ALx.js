b79d69a10391f1a6c44a096c365e9debf8393bfe
blumed <cullan.luther@gmail.com>
2024-10-04 17:05:26 -0500

    moving open props to local package

--- docs/_app/immutable/entry/start.BsdW8ALx.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.xpp565LE.js";export{t as start};
--- src/routes/press/+page.svelte
+++ src/routes/press/+page.svelte
@@ -20,21 +20,21 @@
             <h3>Northfield News</h3>
             <time>Sep 30, 2018</time>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, veritatis laborum libero.</p>
-            <a class="btn" href="https://www.southernminn.com/northfield_news/news/a-great-day-from-family-home-to-5-acre-escape-for-developmentally-disabled/article_245e388f-d84f-57d6-9ac9-a1370db98ee8.html">Read Article</a>
+            <a class="btn" href="https://www.southernminn.com/northfield_news/news/a-great-day-from-family-home-to-5-acre-escape-for-developmentally-disabled/article_245e388f-d84f-57d6-9ac9-a1370db98ee8.html" target="_blank" rel="noopener noreferrer">Read Article</a>
         </article>
         <article class="card">
             <span class="card-type"><IconNews /></span>
             <h3>Pioneer Press</h3>
             <time>Nov 1, 2018</time>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, veritatis laborum libero.</p>
-            <a class="btn" href="https://www.twincities.com/2018/09/16/parents-of-disabled-kids-turn-northfield-farm-into-free-rec-facility-for-others">Read Article</a>
+            <a class="btn" href="https://www.twincities.com/2018/09/16/parents-of-disabled-kids-turn-northfield-farm-into-free-rec-facility-for-others" target="_blank" rel="noopener noreferrer">Read Article</a>
         </article>
         <article class="card">
             <span class="card-type"><IconMic /></span>
             <h3>KYMN Radio</h3>
             <time>Aug 7, 2018</time>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, veritatis laborum libero.</p>
-            <a class="btn" href="https://kymnradio.net/2018/08/07/greg-closser/">Listen to Interview</a>
+            <a class="btn" href="https://kymnradio.net/2018/08/07/greg-closser/" target="_blank" rel="noopener noreferrer">Listen to Interview</a>
         </article>     
         <article class="card connect">
             <h3>Feature us in your next article</h3>
